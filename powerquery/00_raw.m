0954a2e3e6526b85c558fd4d609182839342ac28
NJ3412 <122598074+NeelamJethoe@users.noreply.github.com>
2026-02-04 16:48:22 +0100

    Make 00_raw portable (use Excel.CurrentWorkbook)

--- powerquery/00_raw.m
+++ powerquery/00_raw.m
@@ -1,9 +1,6 @@
 let
-  Source = Excel.Workbook(File.Contents("/Users/aliasaccount/Library/CloudStorage/OneDrive-Personal/Documenten/2025 Bezoekrapportage.xlsx"), null, true),
-  #"Navigation 1" = Source{[Item = "202501", Kind = "Sheet"]}[Data],
-  #"Promoted headers" = Table.PromoteHeaders(#"Navigation 1", [PromoteAllScalars = true]),
-  #"Changed column type" = Table.TransformColumnTypes(#"Promoted headers", {{"CustomerID", type text}, {"Leeftijd", Int64.Type}, {"Abonnement / addon", type text}, {"Binnen geweest op", type datetime}, {"Poort", type text}, {"Ratio", type text}}),
-  #"Removed columns" = Table.RemoveColumns(#"Changed column type", {"Poort", "Ratio"}),
-  #"Changed column type 1" = Table.TransformColumnTypes(#"Removed columns", {{"CustomerID", type text}, {"Leeftijd", Int64.Type}, {"Binnen geweest op", type datetime}, {"Abonnement / addon", type text}})
+  Source = Excel.CurrentWorkbook(){[Name="tbl_checkins"]}[Content],
+  #"Changed column type" = Table.TransformColumnTypes(Source, {{"CustomerID", type text}, {"Leeftijd", Int64.Type}, {"Abonnement / addon", type text}, {"Binnen geweest op", type datetime}}),
+  #"Removed columns" = Table.RemoveColumns(#"Changed column type", {"Poort", "Ratio"})
 in
-  #"Changed column type 1"
+  #"Removed columns"
